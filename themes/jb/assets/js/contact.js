d4fff2d99c869b20c9a94ef6e5aac0f9f98a2dae
Ryan Goodwin <goodwinryan2@gmail.com>
2024-01-09 23:30:16 -0500

    Fix form text color

--- themes/jb/assets/js/contact.js
+++ themes/jb/assets/js/contact.js
@@ -1,23 +1,22 @@
 const wufooFormUrl = new URL("https://jblive.wufoo.com/forms/w7x2r7/")
 
 async function fetchIdStamp() {
-    try {
-      const response = await fetch(wufooFormUrl);
-      
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+  try {
+    const response = await fetch(wufooFormUrl);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
   
-      const data = new DOMParser().parseFromString(await response.text(), "text/html");
-      idstamp = data.getElementById("idstamp");
+    const data = new DOMParser().parseFromString(await response.text(), "text/html");
+    idstamp = data.getElementById("idstamp");
       return idstamp.value;
 
-    } catch (error) {
-      console.error('Fetch error:', error);
-    }
+  } catch (error) {
+    console.error('Fetch error:', error);
   }
+}
   
-  fetchIdStamp().then((data) => {
-    // This is where you can use 'data' outside the function, after the fetch has completed
-    console.log(data);
-  });
+fetchIdStamp().then((data) => {
+  console.log(data);
+});
