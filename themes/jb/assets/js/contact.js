8a8c18b8af89fa8a40fedbf0e09f4ff0b33ec4ef
Ryan Goodwin <goodwinryan2@gmail.com>
2024-01-11 19:15:45 -0500

    continue learning about forms

--- themes/jb/assets/js/contact.js
+++ themes/jb/assets/js/contact.js
@@ -1,4 +1,5 @@
 const wufooFormUrl = new URL("https://jblive.wufoo.com/forms/w7x2r7/")
+const form = document.querySelector("#contact-form")
 
 async function fetchIdStamp() {
   try {
@@ -16,7 +17,27 @@ async function fetchIdStamp() {
     console.error('Fetch error:', error);
   }
 }
-  
-fetchIdStamp().then((data) => {
-  console.log(data);
-});
+
+// reference: https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
+async function sendData() {
+  fetchIdStamp().then((data)=> {
+    idstamp = data;
+  })
+
+  const formData = new FormData(form);
+  formData.append("idstamp", idstamp)
+
+  const response = await fetch("UPDATE ME", {
+    method: "POST",
+    body: formData
+  });
+
+  if (!response.ok) {
+    window.location.href =`${wufooFormUrl}`;
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+})
